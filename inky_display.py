f7b5dd16c52b0ed2aab679430300c723f0c15ae8
Alex Donaldson <alex.donaldson@gmail.com>
2025-05-15 23:59:53 -0700

    get inky to print bme data

--- inky_display.py
+++ inky_display.py
@@ -1,3 +1,5 @@
+import time
+from bme import BME688Sensor
 from inky.auto import auto
 from PIL import Image, ImageFont, ImageDraw
 
@@ -47,11 +49,26 @@ def main():
     """
     print("Initializing Inky display...")
     inky = InkyDisplay()
+    print("Initializing BME688 sensor...")
+    bme688 = BME688Sensor()
 
+    while True:
+        data = bme688.read_data()
+        
+        message = (f"Temperature C: {data['temperature']:.2f} °C")
+        temperature_f = (data['temperature'] * 9 / 5) + 32
+        message = message + (f"\nTemperature F: {temperature_f:.2f} °F")
+        message = message + (f"\nHumidity: {data['humidity']:.2f} %")
+        message = message + (f"\nPressure: {data['pressure']:.2f} hPa")
+        message = message + (f"\nGas Resistance: {data['gas_resistance']:.2f} Ohms")
+        message = message + (f"\nRelative Humidity: {data['relative_humidity']:.2f} %")
+        message = message + (f"\nAltitude: {data['altitude']:.2f} m")
+        inky.print_message(message)
+        time.sleep(3600)  # Wait for 2 seconds before reading again
     # Example message
-    message = "Hello, Inky!"
-    print(f"Displaying message: {message}")
-    inky.print_message(message)
+    # message = "Hello, Inky!"
+    # print(f"Displaying message: {message}")
+    # inky.print_message(message)
 
 if __name__ == "__main__":
     main()
