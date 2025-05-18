9f680d74a49332bed5b33790f1597c75dc320be6
Alex Donaldson <alex.donaldson@gmail.com>
2025-05-17 22:17:16 -0700

    fixed hourly forecast

--- inky_display.py
+++ inky_display.py
@@ -21,9 +21,10 @@ class InkyDisplay:
         self.image = self.background.copy()
         self.draw = ImageDraw.Draw(self.image)
         # Adjust font paths/sizes as needed for your system
-        self.font_large = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 60)
-        self.font_med = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 36)
-        self.font_small = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 24)
+        self.font_large = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 50)
+        self.font_med = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 30)
+        self.font_small = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 20)
+        self.font_xsmall = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 15)
 
     def clear(self):
         self.image = self.background.copy()
@@ -32,18 +33,18 @@ class InkyDisplay:
     def render(self, weather, aqi, bme, sgp30):
         self.clear()
         # --- Center: Current Weather (smaller font) ---
-        x_c, y_c = 320, 60  # Move center block right
+        x_c, y_c = 300, 60  # Move center block right
         self.draw.text((x_c, y_c), f"{weather['current_temp']}°F", self.display.BLACK, font=self.font_large)
         self.draw.text((x_c, y_c+60), f"{weather['current_desc']}", self.display.BLACK, font=self.font_med)
         self.draw.text((x_c, y_c+110), "Indoor Sensors", self.display.BLACK, font=self.font_small)
         bme_temp_f = (bme['temperature'] * 9 / 5) + 32
-        self.draw.text((x_c, y_c+140), f"Temp: {bme_temp_f:.1f}°F", self.display.BLACK, font=self.font_small)
-        self.draw.text((x_c, y_c+170), f"Humidity: {bme['humidity']:.0f}%", self.display.BLACK, font=self.font_small)
-        self.draw.text((x_c, y_c+200), f"Pressure: {bme['pressure']:.0f} hPa", self.display.BLACK, font=self.font_small)
-        self.draw.text((x_c, y_c+230), f"eCO2: {sgp30['eCO2']} ppm", self.display.BLACK, font=self.font_small)
-        self.draw.text((x_c, y_c+260), f"TVOC: {sgp30['TVOC']} ppb", self.display.BLACK, font=self.font_small)
+        self.draw.text((x_c + 2, y_c+140), f"Temp: {bme_temp_f:.1f}°F", self.display.BLACK, font=self.font_small)
+        self.draw.text((x_c + 2, y_c+170), f"Humidity: {bme['humidity']:.0f}%", self.display.BLACK, font=self.font_small)
+        self.draw.text((x_c + 2, y_c+200), f"Pressure: {bme['pressure']:.0f} hPa", self.display.BLACK, font=self.font_small)
+        self.draw.text((x_c + 2, y_c+230), f"eCO2: {sgp30['eCO2']} ppm", self.display.BLACK, font=self.font_small)
+        self.draw.text((x_c + 2, y_c+260), f"TVOC: {sgp30['TVOC']} ppb", self.display.BLACK, font=self.font_small)
         timestamp = datetime.now().strftime("Updated: %Y-%m-%d %H:%M")
-        self.draw.text((x_c, y_c+290), timestamp, self.display.BLACK, font=self.font_small)
+        self.draw.text((x_c, y_c+290), timestamp, self.display.BLACK, font=self.font_xsmall)
 
         # --- Right: 6-day Forecast ---
         x_r, y_r = 600, 40
@@ -61,18 +62,19 @@ class InkyDisplay:
 
         # --- Left: 12-hour Hourly Forecast (smaller font, fits vertically) ---
         x_l, y_l = 20, 40
-        self.draw.text((x_l, y_l), "Next 12 Hours", self.display.BLACK, font=self.font_small)
+        self.draw.text((x_l, y_l), "Next 12 Hours", self.display.BLACK, font=self.font_med)
         y_l += 28
+        print("Hourly forecast data:", weather['hourly'])
         for hour in weather['hourly'][:12]:
-            self.draw.text((x_l, y_l), f"{hour['time']}", self.display.BLACK, font=self.font_small)
-            self.draw.text((x_l+70, y_l), f"{hour['temp']}°", self.display.BLACK, font=self.font_small)
-            self.draw.text((x_l+130, y_l), f"P:{hour.get('precip', '--')}%", self.display.BLACK, font=self.font_small)
+            self.draw.text((x_l, y_l), f"{hour['hour']}:00", self.display.BLACK, font=self.font_small)
+            self.draw.text((x_l+70, y_l), f"{hour['temperature']}°", self.display.BLACK, font=self.font_small)
+            self.draw.text((x_l+130, y_l), f"P:{hour.get('probabilityOfPrecipitation', '--')}%", self.display.BLACK, font=self.font_small)
             y_l += 28
 
         # --- Bottom: Sunrise/Sunset ---
         sunrise = weather.get('sunrise', '--:--')
         sunset = weather.get('sunset', '--:--')
-        self.draw.text((self.width//2-200, self.height-60), f"Sunrise: {sunrise}   Sunset: {sunset}", self.display.BLACK, font=self.font_med)
+        self.draw.text((self.width//2-200, self.height-60), f"Sunrise: {sunrise}   Sunset: {sunset}", self.display.BLACK, font=self.font_small)
 
         self.display.set_image(self.image)
         self.display.show()
--- weather_gov.py
+++ weather_gov.py
@@ -6,6 +6,7 @@ from datetime import datetime, timezone, timedelta
 
 # Constants
 DAILY_FORECAST_URL = "https://api.weather.gov/gridpoints/{gridId}/{gridX},{gridY}/forecast"
+HOURLY_FORECAST_URL = "https://api.weather.gov/gridpoints/{gridId}/{gridX},{gridY}/forecast/hourly"
 WEATHER_TIME_FORMAT = "%Y-%m-%dT%H:%M:%S%z"
 
 class RemoteWeather:
@@ -33,16 +34,33 @@ class RemoteWeather:
         self.grid_id = properties["gridId"]
         self.grid_x = properties["gridX"]
         self.grid_y = properties["gridY"]
-        self.daily_forecast_url = DAILY_FORECAST_URL.format(
-            gridId=self.grid_id, gridX=self.grid_x, gridY=self.grid_y
-        )
+        print(self.daily_forecast_url)
 
     def get_raw_daily_forecast_data(self):
         """
         Fetch raw daily forecast data from the API.
         """
-        response = urllib.request.urlopen(self.daily_forecast_url)
+        daily_forecast_url = DAILY_FORECAST_URL.format(
+            gridId=self.grid_id, gridX=self.grid_x, gridY=self.grid_y
+        )
+        return self.get_raw_forecast_data(daily_forecast_url)
+    
+    def get_raw_forecast_data(self, url):
+        """
+        Fetch raw forecast data from the API.
+        """
+        response = urllib.request.urlopen(url)
         return json.loads(response.read().decode("utf8"))
+    
+    def get_raw_hourly_forecast_data(self):
+        """
+        Fetch raw hourly forecast data from the API.
+        """
+        hourly_forecast_url = HOURLY_FORECAST_URL.format(
+            gridId=self.grid_id, gridX=self.grid_x, gridY=self.grid_y
+        )
+        return self.get_raw_forecast_data(hourly_forecast_url)
+    
 
     def get_daily_forecast(self):
         """
@@ -73,13 +91,14 @@ class RemoteWeather:
         hourly_forecast = []
 
         for period in periods:
-            if "hour" in period["name"].lower():
-                hourly_forecast.append({
-                    "time": datetime.strptime(period["startTime"], WEATHER_TIME_FORMAT).strftime("%I:%M %p"),
-                    "temperature": period["temperature"],
-                    "wind_speed": period["windSpeed"],
-                    "short_forecast": period["shortForecast"]
-                })
+            hourly_forecast.append({
+                "hour": datetime.strptime(period["startTime"], WEATHER_TIME_FORMAT).strftime("%H"),
+                "temperature": period["temperature"],
+                "wind_speed": period["windSpeed"],
+                "wind_direction": period["windDirection"],
+                "short_forecast": period["shortForecast"],
+                "probabilityOfPrecipitation": period["probabilityOfPrecipitation"]["value"]
+            })
         return hourly_forecast
 
 
@@ -89,7 +108,7 @@ class RemoteWeather:
         """
         current_conditions = self.get_raw_daily_forecast_data()["properties"]["periods"][0]
         return {
-            "temperature": current_conditions["temperature"],
+            "temp": current_conditions["temperature"],
             "wind_speed": current_conditions["windSpeed"],
             "short_forecast": current_conditions["shortForecast"],
             "temp_unit": current_conditions["temperatureUnit"],
@@ -128,6 +147,15 @@ def main():
     daily_forecast = weather.get_daily_forecast()
     print(weather.get_current_weather())
 
+    print("\nHourly Forecast:")
+    hourly_forecast = weather.get_hourly_forecast()
+    for hour in hourly_forecast:
+        print(f"Time: {hour['hour']}:00")
+        print(f"  Temp: {hour['temp']}°F")
+        print(f"  Wind Speed: {hour['wind_speed']}")
+        print(f"  Wind Direction: {hour['wind_direction']}")
+        print(f"  Forecast: {hour['short_forecast']}")
+        print()
     # print("[7-Day Weather Forecast]")
     # for day in daily_forecast:
     #     print(f"Date: {day['date']}")
